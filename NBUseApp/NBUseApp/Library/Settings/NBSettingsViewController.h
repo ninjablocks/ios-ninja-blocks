7b75003f438fc20daa38815b8c2414559cd8511d
James <james.zaki@gmail.com>
2012-11-30 16:52:46 +1100

    settings view controller as table view controller

--- NBUseApp/NBUseApp/Library/Settings/NBSettingsViewController.h
+++ NBUseApp/NBUseApp/Library/Settings/NBSettingsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NBSettingsViewController : UIViewController
+@interface NBSettingsViewController : UITableViewController
 
 @end
--- NBUseApp/NBUseApp/Library/Settings/NBSettingsViewController.m
+++ NBUseApp/NBUseApp/Library/Settings/NBSettingsViewController.m
@@ -8,17 +8,29 @@
 
 #import "NBSettingsViewController.h"
 
+#import "NBDeviceManager.h"
+
+#import "NBDeviceHWInterface.h"
+#import "NBDevice.h"
+
+#import "NBDeviceSettingsTableViewCell.h"
+
 @interface NBSettingsViewController ()
+{
+    NBDeviceManager *deviceManager;
+}
+
+@property (strong, nonatomic) IBOutlet NBDeviceSettingsTableViewCell *deviceCell;
 
 @end
 
 @implementation NBSettingsViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        deviceManager = [NBDeviceManager sharedManager];
     }
     return self;
 }
@@ -26,7 +38,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +51,101 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return [[deviceManager interfaces] count];
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSArray *interfaces = deviceManager.interfaces;
+    NBDeviceHWInterface *interface = [interfaces objectAtIndex:section];
+    return interface.interfaceName;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    NBDeviceHWInterface *interface = [[deviceManager interfaces] objectAtIndex:section];
+    return [[interface devices] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NBDeviceSettingsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[NBDeviceSettingsTableViewCell reuseIdentifier]];
+    if (cell == nil) {
+        [[NSBundle mainBundle] loadNibNamed:@"NBDeviceSettingsTableViewCell"
+                                      owner:self
+                                    options:nil
+         ];
+        cell = self.deviceCell;
+        self.deviceCell = nil;
+    }
+    NBDeviceHWInterface *interface = [[deviceManager interfaces] objectAtIndex:indexPath.section];
+    NBDevice *device = [[interface devices] objectAtIndex:indexPath.row];
+
+    [cell setupWithDevice:device];
+    //TODO: disable
+    //TODO: add toggle to change active/inactive
+    
+    return cell;
+}
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ }
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+}
+
 
 @end
