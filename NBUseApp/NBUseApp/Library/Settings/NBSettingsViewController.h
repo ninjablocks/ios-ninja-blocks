c7ec0e12f31c967a07bc2d0d40ae6644b21b731b
James <james.zaki@gmail.com>
2012-11-30 11:12:37 +1100

    added setting view controller

---
+++ NBUseApp/NBUseApp/Library/Settings/NBSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NBSettingsViewController.h
+//  NBUseApp
+//
+//  Created by jz on 30/11/12.
+//  Copyright (c) 2012 James Zaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBSettingsViewController : UIViewController
+
+@end
