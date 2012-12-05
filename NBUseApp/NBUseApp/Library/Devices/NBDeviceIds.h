69c4190a6d3bad00394f15f00943f0b5a230c2fd
James <james.zaki@gmail.com>
2012-12-05 18:43:59 +1100

    preliminary device id updates

--- NBUseApp/NBUseApp/Library/Devices/NBDeviceIds.h
+++ NBUseApp/NBUseApp/Library/Devices/NBDeviceIds.h
@@ -12,20 +12,38 @@
 #define kVendorNinjaBlocks 0
 
 typedef enum {
-    kNBDIDAccelerometer             = 2
+    //TODO: use these once sql is updated on staging
+//    kNBDIDPushButton                = 200
+//    , kNBDIDAccelerometer           = 206
+//    , kNBDIDOrientation             = 207
+//    , kNBDIDAccelerometerState      = 208
+//    , kNBDIDNorthHeading            = 221
+//    , kNBDIDLocation                = 222
+//    , kNBDIDLEDUser                 = 1000
+//    , kNBDIDWebcam                  = 1004
+  
+    kNBDIDTemperature               = 1 //not implemented
+    , kNBDIDAccelerometer           = 2
     , kNBDIDAccelerometerState      = 3
     , kNBDIDOrientation             = 4
     , kNBDIDPushButton              = 5
-    , kNBDIDLightSensor             = 6 //not yet implemented
-    , kNBDIDPassiveInfrared         = 7
+    , kNBDIDLightSensor             = 6 //not implemented
+    , kNBDIDPassiveInfrared         = 7 //not implemented
+    , kNBDIDHumiditySensor          = 8 //not implemented
     , kNBDIDTemperatureDHT22        = 9
+    , kNBDIDDistanceSensor          = 10 //not implemented
+    , kNBDIDRF433                   = 11 //not implemented
     , kNBDIDSoundSensor             = 12
+    , kNBDIDTemperatureLC           = 13 //not implemented
+    , kNBDIDHIDDevice               = 14 //not implemented
+    , kNBDIDRFIDReader              = 15 //not implemented
+    , kNBDIDServo                   = 16 //not implemented
     , kNBDIDNorthHeading            = 15
     , kNBDIDLocation                = 16
     , kNBDIDLEDBoard                = 999
     , kNBDIDLEDUser                 = 1000
     , kNBDIDWebcam                  = 1004
-    
+
 } NBDeviceId;
 
 #endif
