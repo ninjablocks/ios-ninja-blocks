bc872e26854d6e30943772437c3ea722e448243f
James Zaki <james.zaki@gmail.com>
2013-01-04 13:48:01 +1100

    updated device ID's to 2xx range

--- NBUseApp/NBUseApp/Library/Devices/NBDeviceIds.h
+++ NBUseApp/NBUseApp/Library/Devices/NBDeviceIds.h
@@ -12,35 +12,13 @@
 #define kVendorNinjaBlocks 0
 
 typedef enum {
-    //TODO: use these once sql is updated on staging
-//    kNBDIDPushButton                = 200
-//    , kNBDIDAccelerometer           = 206
-//    , kNBDIDOrientation             = 207
-//    , kNBDIDAccelerometerState      = 208
-//    , kNBDIDHIDDevice               = 216
-//    , kNBDIDNorthHeading            = 221
-//    , kNBDIDLocation                = 222
-//    , kNBDIDLEDUser                 = 1000
-//    , kNBDIDWebcam                  = 1004
-  
-    kNBDIDTemperature               = 1 //not implemented
-    , kNBDIDAccelerometer           = 2
-    , kNBDIDAccelerometerState      = 3
-    , kNBDIDOrientation             = 4
-    , kNBDIDPushButton              = 5
-    , kNBDIDLightSensor             = 6 //not implemented
-    , kNBDIDPassiveInfrared         = 7 //not implemented
-    , kNBDIDHumiditySensor          = 8 //not implemented
-    , kNBDIDTemperatureDHT22        = 9
-    , kNBDIDDistanceSensor          = 10 //not implemented
-    , kNBDIDRF433                   = 11 //not implemented
-    , kNBDIDSoundSensor             = 12
-    , kNBDIDTemperatureLC           = 13 //not implemented
-    , kNBDIDHIDDevice               = 14
-    , kNBDIDRFIDReader              = 15 //not implemented
-    , kNBDIDServo                   = 16 //not implemented
-    , kNBDIDNorthHeading            = 15
-    , kNBDIDLocation                = 16
+    kNBDIDPushButton                = 200   //
+    , kNBDIDOrientation             = 208   //phone orientation
+    , kNBDIDAccelerometerState      = 209   //"shake" "jiggle"
+    , kNBDIDLightSensor             = 224   //not implemented
+    , kNBDIDHIDDevice               = 210   //gestures
+    , kNBDIDNorthHeading            = 222
+    , kNBDIDLocation                = 223
     , kNBDIDLEDBoard                = 999
     , kNBDIDLEDUser                 = 1000
     , kNBDIDWebcam                  = 1004
